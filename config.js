93b1f814b68eeecb531df3121c03eabe678a23e9
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-09 21:00:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    token: process.env.TOKEN,
+
+    clientId: '1392827175815614504',
+
+    // channels
+    applicationChannelId: '1513868095263871026',
+    interviewChannelId: '1513868095263871026',
+    renameChannelId: '1513868181393903646', // optional (same channel ok)
+
+    // roles
+    moderatorRoleId: '1513868317092216983',
+    unverifiedRoleId: '1513868625533210655',
+    citizenRoleId: '1513868598463172668',
+
+    // system settings (optional but useful later)
+    logsChannelId: null, // lagay mo kung gusto mo logs
+};
