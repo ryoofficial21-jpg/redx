4141b9a6f4ee84a0a03710f91c54eb76d605245d
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-11 02:52:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    token: process.env.TOKEN,
+
+    clientId: '1513530505734389862',
+
+    // channels
+    applicationChannelId: '1513509438466691264',
+    interviewChannelId: '1513509438466691264',
+
+    // roles
+    moderatorRoleId: '1513509664971817020',
+    unverifiedRoleId: '1513509723532558456',
+    citizenRoleId: '1513509705232683068',
+
+    logChannelId: '1514341104059809873',
+};
