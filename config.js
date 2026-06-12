279c10b13e8fa86b2998620bd3e8e2036f8d7fda
ryoofficial21-jpg <ryoofficial21@gmail.com>
2026-06-12 21:26:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    token: process.env.TOKEN,
+
+    clientId: '1513530505734389862',
+
+    // channels
+    applicationChannelId: '1513509438466691264', // where users apply
+    interviewChannelId: '1513509438466691264', // where interview embeds go
+
+    // roles
+    moderatorRoleId: '1513509664971817020',
+    unverifiedRoleId: '1513509723532558456',
+    citizenRoleId: '1513509705232683068',
+    adminRoleId: '1513509662505304106',
+    
+    guildId: '1513505366187446466',
+};
